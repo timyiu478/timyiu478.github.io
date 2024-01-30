5ac5f451b9d5c24fac23002b07dc8ae6ece84b6b
tim <ctom3560@gmail.com>
2024-01-30 21:13:40 +0800

    add post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706434824';
+const cacheName = 'chirpy-1706620396';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
