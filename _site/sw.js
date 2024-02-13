a4546851731e40de066484d569453399f734dc5f
tim <ctom3560@gmail.com>
2024-02-13 19:27:01 +0800

    fix typo

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707805864';
+const cacheName = 'chirpy-1707823598';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
