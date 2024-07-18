2661bda6337f8ea14a9d6d2dddee05df7f45bc09
tim <ctom3560@gmail.com>
2024-07-18 17:45:11 +0800

    add sub title

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1721295373';
+const cacheName = 'chirpy-1721295886';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
