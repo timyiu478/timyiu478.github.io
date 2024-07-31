164ed485eb4bb91ad1aa91aa500b1f1d6ac41566
tim <ctom3560@gmail.com>
2024-07-31 12:43:54 +0800

    fix grammer

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1722399598';
+const cacheName = 'chirpy-1722401014';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
